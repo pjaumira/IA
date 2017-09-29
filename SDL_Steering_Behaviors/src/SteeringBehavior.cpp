1fe1be9875fe8b232f6cc6009b162611d204028a
Eloi Salles Torres <esalles@enti.local>
2017-09-29 16:48:37 +0200

    First Commit

---
+++ SDL_Steering_Behaviors/src/SteeringBehavior.cpp
@@ -0,0 +1,58 @@
+#include "SteeringBehavior.h"
+
+
+
+SteeringBehavior::SteeringBehavior()
+{
+}
+
+
+SteeringBehavior::~SteeringBehavior()
+{
+}
+
+Vector2D SteeringBehavior::KinematicSeek(Agent *agent, Vector2D target, float dtime)
+{
+	Vector2D steering = target - agent->position;
+	steering.Normalize();
+	return steering * agent->max_velocity;
+}
+
+Vector2D SteeringBehavior::KinematicSeek(Agent *agent, Agent *target, float dtime)
+{
+	return KinematicSeek(agent, target->position, dtime);
+}
+
+Vector2D SteeringBehavior::KinematicFlee(Agent *agent, Vector2D target, float dtime)
+{
+	Vector2D steering = agent->position - target;
+	steering.Normalize();
+	return steering * agent->max_velocity;
+}
+
+Vector2D SteeringBehavior::KinematicFlee(Agent *agent, Agent *target, float dtime)
+{
+	return KinematicFlee(agent, target->position, dtime);
+}
+
+/* Add here your own Steering Behavior functions definitions */
+
+Vector2D SteeringBehavior::Seek(Agent *agent, Vector2D target, float dtime)
+{
+	return Vector2D(0, 0);
+}
+
+Vector2D SteeringBehavior::Seek(Agent *agent, Agent *target, float dtime)
+{
+	return Seek(agent, target->position, dtime);
+}
+
+Vector2D SteeringBehavior::Flee(Agent *agent, Vector2D target, float dtime)
+{
+	return Vector2D(0,0);
+}
+
+Vector2D SteeringBehavior::Flee(Agent *agent, Agent *target, float dtime)
+{
+	return Flee(agent, target->position, dtime);
+}
