0b37dd9fc5e1356c1a94a4fefb007a8612f00d45
Portatil <togirainoroka@gmail.com>
2017-09-30 12:34:31 +0200

    1.1
    Part de Steering Behaviour.h feta

--- SDL_Steering_Behaviors/src/SteeringBehavior.h
+++ SDL_Steering_Behaviors/src/SteeringBehavior.h
@@ -15,10 +15,28 @@ public:
 	Vector2D KinematicFlee(Agent *agent, Agent *target, float dtime);
 
 	/* Add here your own Steering Behavior functions declarations */
+
+	// Seek
 	Vector2D Seek(Agent *agent, Vector2D target, float dtime);
 	Vector2D Seek(Agent *agent, Agent *target, float dtime);
+
+	//Flee
 	Vector2D Flee(Agent *agent, Vector2D target, float dtime);
 	Vector2D Flee(Agent *agent, Agent *target, float dtime);
-	//etc...
 
+	//Arrive
+	Vector2D Arrive(Agent *agent, Vector2D target, float dtime);
+	Vector2D Arrive(Agent *agent, Agent *target, float dtime);
+
+	//Pursue
+	Vector2D Pursue(Agent *agent, Vector2D target, float dtime);
+	Vector2D Pursue(Agent *agent, Agent *target, float dtime);
+
+	//Evade
+	Vector2D Evade(Agent *agent, Vector2D target, float dtime);
+	Vector2D Evade(Agent *agent, Agent *target, float dtime);
+
+	//Wander
+	Vector2D Wander(Agent *agent, Vector2D target, float dtime);
+	Vector2D Wander(Agent *agent, Agent *target, float dtime);
 };
